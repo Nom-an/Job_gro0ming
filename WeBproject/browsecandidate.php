b998a89cc913e7fb3f940b6a542a91378b4ac4ae
MD NOMAN AKANDA <120173010+Nom-an@users.noreply.github.com>
2026-03-08 10:09:13 +0600

    Add files via upload

---
+++ WeBproject/browsecandidate.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="browsecandidate.css">
+</head>
+
+<body>
+	<header>
+		<img src="asset/messenger.png" style="width: 30px; height: 30px;">
+		<img src="asset/bell.png" style="width: 30px; height: 30px;">
+		<img src="asset/instagram.png" style="width: 30px; height: 30px;">
+
+		<a id="home" href="http://127.0.0.1:5500/sujoggg1/index.html">Home</a>
+
+	</header>
+	<nav>
+
+		<li><a href="#"><h2>Candidates</h2></a></li>
+
+	</nav>
+	<section class="search">
+		<form action="#" method="get">
+
+		<input type="text" name="keywords"
+				placeholder="Job title, Location" id="myInput" onkeyup="myFunction()">
+
+			<button type="submit">
+				Find Job
+			</button>
+		</form>
+	</section>
+
+	<section class="job-listings">
+		<ul id="myUL"> 
+			<li style="background-image: url('asset/pmain.png');"> 
+				<h3>Brainstation 23</h3>
+				<p >Marketing Officer  <br> Dhaka <br> 2 Years experience </p>
+				<a href="https://brainstation-23.com/career-verse/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p0.png');"> 
+				<h3>Cefalo</h3>
+				<p>Interaction Designer <br> Dhaka <br> 3 Years experience</p> 
+				<a href="https://career.cefalo.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p1.png');"> 
+				<h3>Dizency</h3> 
+				<p>Graphic Designer <br> Chittagong <br> 1 Years experience </p> 
+				<a href="https://dizency.com/career/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p2.png');"> 
+				<h3>Enosis</h3> 
+				<p>Senior Software Engineer <br> Dhaka <br> 3 Years experience</p> 
+				<a href="https://enosisbd.pinpointhq.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>Neosysworld </h3> 
+				<p>Marketing Officer <br> Chittagong <br> 5 Years experience</p> 
+				<a href="https://www.neosysworld.com/career-at-neosysworld-chittagong.html">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p3.png');"> 
+				<h3>Zeropoint Computing</h3> 
+				<p>Interaction Designer <br> Rajshahi <br> 1 Years experience</p> 
+				<a href="https://www.zeropointcomputing.com/career/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>The Student IT</h3> 
+				<p>Junior Graphic Designer <br> Rajshahi <br> 2 Years experience</p> 
+				<a href="https://www.thestudentcompany.net/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>Byreached</h3> 
+				<p>Senior UX Designer <br> Barisal <br> 3 Years experience</p> 
+				<a href="https://byreached.com/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p3.png');"> 
+				<h3>Pro SEO BD</h3> 
+				<p>SEO Expert <br> Barisal <br> 1 Years experience</p> 
+				<a href="https://www.webseoprobd.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>Tent Communications </h3> 
+				<p>UX Designer <br> Sylhet <br> 3 Years experience</p> 
+				<a href="https://tentcomm.com/career/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/pmain.png');"> 
+				<h3>Technext </h3> 
+				<p>Senior UX Designer <br> Sylhet <br> 1 Years experience</p> 
+				<a href="https://technext.it/careers/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/pmain.png');"> 
+				<h3>Brainstation 23</h3>
+				<p >Marketing Officer  <br> Dhaka <br> 2 Years experience </p>
+				<a href="https://brainstation-23.com/career-verse/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p0.png');"> 
+				<h3>Cefalo</h3>
+				<p>Interaction Designer <br> Dhaka <br> 3 Years experience</p> 
+				<a href="https://career.cefalo.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p1.png');"> 
+				<h3>Dizency</h3> 
+				<p>Graphic Designer <br> Chittagong <br> 1 Years experience </p> 
+				<a href="https://dizency.com/career/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p2.png');"> 
+				<h3>Enosis</h3> 
+				<p>Senior Software Engineer <br> Dhaka <br> 3 Years experience</p> 
+				<a href="https://enosisbd.pinpointhq.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>Neosysworld </h3> 
+				<p>Marketing Officer <br> Chittagong <br> 5 Years experience</p> 
+				<a href="https://www.neosysworld.com/career-at-neosysworld-chittagong.html">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p3.png');"> 
+				<h3>Zeropoint Computing</h3> 
+				<p>Interaction Designer <br> Rajshahi <br> 1 Years experience</p> 
+				<a href="https://www.zeropointcomputing.com/career/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>The Student IT</h3> 
+				<p>Junior Graphic Designer <br> Rajshahi <br> 2 Years experience</p> 
+				<a href="https://www.thestudentcompany.net/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>Byreached</h3> 
+				<p>Senior UX Designer <br> Barisal <br> 3 Years experience</p> 
+				<a href="https://byreached.com/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/p3.png');"> 
+				<h3>Pro SEO BD</h3> 
+				<p>SEO Expert <br> Barisal <br> 1 Years experience</p> 
+				<a href="https://www.webseoprobd.com/">View Career</a> 
+			</li>
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>Tent Communications </h3> 
+				<p>UX Designer <br> Sylhet <br> 3 Years experience</p> 
+				<a href="https://tentcomm.com/career/">View Career</a> 
+			</li> 
+			<li style="background-image: url('asset/pmain.png');"> 
+				<h3>Technext </h3> 
+				<p>Senior UX Designer <br> Sylhet <br> 1 Years experience</p> 
+				<a href="https://technext.it/careers/">View Career</a> 
+			</li> 
+		</ul>
+	</section>
+
+	<script>
+		function myFunction() {
+			var input, filter, ul, li, p, i, txtValue;
+			input = document.getElementById("myInput");
+			filter = input.value.toUpperCase();
+			ul = document.getElementById("myUL");
+			li = ul.getElementsByTagName("li");
+			for (i = 0; i < li.length; i++) {
+				p = li[i].getElementsByTagName("p")[0];
+				txtValue = p.textContent || p.innerText;
+				if (txtValue.toUpperCase().indexOf(filter) > -1) {
+					li[i].style.display = "";
+				} else {
+					li[i].style.display = "none";
+				}
+			}
+		}
+		</script>
+
+	<footer>
+		<p>© 2024 JoB grooming & placement </p>
+	</footer>
+</body>
+
+</html>
---
+++ WeBproject/employersdasboard.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="employersdasboard.css">
+</head>
+
+<body>
+	<header>
+		<img src="asset/messenger.png" style="width: 30px; height: 30px;">
+		<img src="asset/bell.png" style="width: 30px; height: 30px;">
+		<img src="asset/instagram.png" style="width: 30px; height: 30px;">
+	</header>
+	<nav>
+
+	<li><a href="#"><h2>Hello</h2> </a></li>
+	<h6>Here is your daily activities and applications</h6> 
+
+	</nav>
+	
+
+	<section class="job-listings">
+		<ul>
+			<h2>Recently Posted Jobs</h2>
+
+			<h4 align="right">  view all</h4>
+			<li style="background-image: url('asset/ux.png');"> 
+				<h3>Senior UX Designer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 27 days remain</h5> 
+				
+				<a href="senioruxdesigner.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>UI/UX Designer</h3> 
+				<h4>Internship</h4> 
+				<h5>Application deadline: 8 days remain</h5> 
+				
+				<a href="uxdesigner.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p0.png');"> 
+				<h3>Technical Support Specialist</h3> 
+				<h4>Part Time</h4> 
+				<h5>Application deadline: 4 days remain</h5> 
+				
+				<a href="technical.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/ux.png');"> 
+				<h3>Junior Graphic Designer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 24 days remain</h5> 
+				
+				<a href="uxdesigner.html"">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>Front End Developer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 7 days remain</h5> 
+				
+				<a href="senioruxdesigner.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/ux.png');"> 
+				<h3>Senior UX Designer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 27 days remain</h5> 
+				
+				<a href="senioruxdesigner.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p4.png');"> 
+				<h3>UI/UX Designer</h3> 
+				<h4>Internship</h4> 
+				<h5>Application deadline: 8 days remain</h5> 
+				
+				<a href="uxdesigner.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p0.png');"> 
+				<h3>Technical Support Specialist</h3> 
+				<h4>Part Time</h4> 
+				<h5>Application deadline: 4 days remain</h5> 
+				
+				<a href="technical.html">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/ux.png');"> 
+				<h3>Junior Graphic Designer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 24 days remain</h5> 
+				
+				<a href="uxdesigner.html"">View Application</a> 
+			</li> 
+			<li style="background-image: url('asset/p5.png');"> 
+				<h3>Front End Developer</h3> 
+				<h4>Full Time</h4> 
+				<h5>Application deadline: 7 days remain</h5> 
+				
+				<a href="senioruxdesigner.html">View Application</a> 
+			</li> 
+
+
+		</ul>
+	</section>
+
+	<footer>
+		<p>© 2024 JoB grooming & placement </p>
+	</footer>
+</body>
+
+</html>
